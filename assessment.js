89b06a78c57dd1af0db64d5f221d7bc705492a1e
Shikiji <56910224+shikij1@users.noreply.github.com>
2020-01-17 21:47:28 +0900

    Enterの処理の追加

--- assessment.js
+++ assessment.js
@@ -39,6 +39,13 @@ assessmentButton.onclick = () => {
   script.setAttribute("src", "https://platform.twitter.com/widgets.js");
   tweetDivided.appendChild(script);
 };
+
+userNameInput.onkeydown = event => {
+  if (event.key === "Enter") {
+    assessmentButton.onclick();
+  }
+};
+
 const answers = [
   "{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。",
   "{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。",
